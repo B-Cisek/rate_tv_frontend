7dcf6b43456528cf90389f9d49a06c97adefd277
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2025-02-06 00:22:27 +0100

    install base modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: false },
+  modules: [
+    "@nuxt/ui",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@nuxtjs/color-mode",
+  ],
+  colorMode: {
+    preference: "system",
+    fallback: "light",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "-mode",
+    storage: "localStorage",
+    storageKey: "nuxt-color-mode",
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="bg-red-600 text-2xl">Index Page</h1>
+</template>
